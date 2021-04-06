22e38d6531820e19b575ad1a5de29b8230e73a00
biswjit9224 <dashbiswa2019@gmail.com>
2021-04-06 12:27:28 -0700

    I changed one line for documention

--- app1/views.py
+++ app1/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# check for github
 
 def My_web(request):
     return HttpResponse("<p>It Is My <u><b>1st Web</u></b> Page In django</p>")
