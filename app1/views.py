78114bc748d2fae8fff766e9359a28264fd6078a
biswjit9224 <dashbiswa2019@gmail.com>
2021-04-06 22:37:20 -0700

    change check

--- app1/views.py
+++ app1/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 # check for github
-
+# git add check
 def My_web(request):
     return HttpResponse("<p>It Is My <u><b>1st Web</u></b> Page In django</p>")
 
